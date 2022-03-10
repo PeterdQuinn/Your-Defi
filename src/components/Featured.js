a42585914f5caf4170331c81cb1a8584d5931149
Your Name <peterdquinnsr@gmail/com>
2022-03-10 15:06:26 -0700

    Featured.js

---
+++ src/components/Featured.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Featured from './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+        <div className='container'>
+              <div className='left'>
+                <h2>Explore Top currencies like Bitcoin, Ethereum, and Solana</h2> 
+                    <p>See all available assests: Defi,NFT's and currencies</p> 
+                    <button className='btn'>See more coins</button>
+                </div>
+                    <div className='right'>
+                     <div className='top'>
+                         
+                        </div>   
+                    </div>       
+        </div>
+    </div>
+  )
+}
+
+export default Featured
