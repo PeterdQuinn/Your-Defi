6fd8b6774f26f5ffe00a1413c7d978bf5790f2db
Your Name <peterdquinnsr@gmail/com>
2022-03-10 16:52:48 -0700

    web site finished

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,9 +12,8 @@ export const Hero = () => {
              <h1>Invest in cypto currency with your retirment.</h1> 
              <p>Buy sell or trade hundred of assests now.</p> 
              <div className='input-container'>
-                  <input type='email' placeholder='Enter your email'>
-                   <button className='btn'>Learn More</button>   
-                </input>      
+             <input type='email' placeholder='Enter email here'/> 
+                   <button className='btn'>Learn More</button>         
             </div>
         </div>   
            <div className='right'>
